d458000e7c1e49f135ebb108cafc856387e6ad28
Justin Bravo <justinbravo@Justins-MacBook-Pro.local>
2024-04-27 15:33:10 -0400

    Pushing code

--- src/main/java/com/shepherdmoney/interviewproject/controller/CreditCardController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/CreditCardController.java
@@ -1,39 +1,60 @@
 package com.shepherdmoney.interviewproject.controller;
 
+import com.shepherdmoney.interviewproject.model.CreditCard;
+import com.shepherdmoney.interviewproject.model.User;
+import com.shepherdmoney.interviewproject.repository.CreditCardRepository;
+import com.shepherdmoney.interviewproject.repository.UserRepository;
 import com.shepherdmoney.interviewproject.vo.request.AddCreditCardToUserPayload;
 import com.shepherdmoney.interviewproject.vo.request.UpdateBalancePayload;
 import com.shepherdmoney.interviewproject.vo.response.CreditCardView;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import java.util.stream.Collectors;
 
 
 @RestController
 public class CreditCardController {
 
-    // TODO: wire in CreditCard repository here (~1 line)
+    private CreditCardRepository creditCardRepository;
+    private UserRepository userRepository;
 
     @PostMapping("/credit-card")
     public ResponseEntity<Integer> addCreditCardToUser(@RequestBody AddCreditCardToUserPayload payload) {
-        // TODO: Create a credit card entity, and then associate that credit card with user with given userId
-        //       Return 200 OK with the credit card id if the user exists and credit card is successfully associated with the user
-        //       Return other appropriate response code for other exception cases
-        //       Do not worry about validating the card number, assume card number could be any arbitrary format and length
-        return null;
+        User user = userRepository.findById(payload.getUserId()).orElse(null);
+        if (user == null) {
+            return ResponseEntity.badRequest().body(400);
+        }
+        CreditCard newCreditCard = new CreditCard();
+        newCreditCard.setIssuanceBank(payload.getCardIssuanceBank());
+        newCreditCard.setNumber(payload.getCardNumber());
+        newCreditCard.setUser(user);
+
+        CreditCard saveCreditCard = creditCardRepository.save(newCreditCard);
+
+        // Return the ID of the newly created credit card
+        return ResponseEntity.ok(saveCreditCard.getId());
     }
 
     @GetMapping("/credit-card:all")
     public ResponseEntity<List<CreditCardView>> getAllCardOfUser(@RequestParam int userId) {
-        // TODO: return a list of all credit card associated with the given userId, using CreditCardView class
-        //       if the user has no credit card, return empty list, never return null
-        return null;
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null || user.getCreditCards().isEmpty()) {
+            // If user does not exist or user has no credit cards return empty List.
+            return ResponseEntity.ok(Collections.emptyList());
+        }
+        List<CreditCardView> cards = user.getCreditCards().stream().
+                map(card -> new CreditCardView(card.getIssuanceBank(), card.getNumber()))
+                .toList();
+        return ResponseEntity.ok(cards);
     }
 
     @GetMapping("/credit-card:user-id")
     public ResponseEntity<Integer> getUserIdForCreditCard(@RequestParam String creditCardNumber) {
+        CreditCard card = creditCardRepository
         // TODO: Given a credit card number, efficiently find whether there is a user associated with the credit card
         //       If so, return the user id in a 200 OK response. If no such user exists, return 400 Bad Request
         return null;
--- src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.shepherdmoney.interviewproject.controller;
 
+import com.shepherdmoney.interviewproject.model.User;
+import com.shepherdmoney.interviewproject.repository.UserRepository;
 import com.shepherdmoney.interviewproject.vo.request.CreateUserPayload;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,20 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
-    // TODO: wire in the user repository (~ 1 line)
+    private UserRepository userRepository;
 
     @PutMapping("/user")
     public ResponseEntity<Integer> createUser(@RequestBody CreateUserPayload payload) {
-        // TODO: Create an user entity with information given in the payload, store it in the database
-        //       and return the id of the user in 200 OK response
-        return null;
+        // Creating a new user
+        User newUser = new User(payload.getName(), payload.getEmail());
+        User saveUser = userRepository.save(newUser);
+        return ResponseEntity.ok(saveUser.getId());
     }
 
     @DeleteMapping("/user")
     public ResponseEntity<String> deleteUser(@RequestParam int userId) {
-        // TODO: Return 200 OK if a user with the given ID exists, and the deletion is successful
-        //       Return 400 Bad Request if a user with the ID does not exist
-        //       The response body could be anything you consider appropriate
-        return null;
+        // Attempt to find the user by ID
+        User user = userRepository.findById(userId).orElse(null);
+        if (user != null) {
+            userRepository.delete(user);
+            return ResponseEntity.ok("User deleted successfully");
+        } else {
+            return ResponseEntity.badRequest().body("User with id: " + userId + " does not exist");
+        }
     }
 }
